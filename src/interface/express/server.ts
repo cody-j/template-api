4b337db5d2cfed2c8fdbdcfc582055223f33907f
Cody J <codywj777@gmail.com>
2024-12-31 17:00:07 -0800

    moves interfaces to /interface

--- src/interface/express/server.ts
+++ src/interface/express/server.ts
@@ -1,11 +1,20 @@
 import express from 'express';
 import cors from 'cors';
 
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// error handler
+// body parser
+// authentication
+// middy + express/lambda? 
+
+
+app.get('/health', (req, res) => {
+    res.status(200).json('ok');
+});
 // app.use('/api', routes);
 
 export default app;
